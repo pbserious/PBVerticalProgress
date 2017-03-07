b12cdaa26b7d2fb1596cffbe0122020e53b34408
Rattee <Rattee_War@truecorp.co.th>
2017-03-07 11:25:58 +0700

    finish simple version

--- PBVerticalProgress/PBVerticalProgress.swift
+++ PBVerticalProgress/PBVerticalProgress.swift
@@ -14,10 +14,97 @@ import UIKit
     var progress = UIView()
     var progressTrack = UIView()
     
-    @IBInspectable var iconImage:UIImage?
-    @IBInspectable var progressValue:Float = 0.0 {
+    @IBInspectable var iconImage:UIImage? {
         didSet {
-            
+            updateImageView()
         }
+    }
+    @IBInspectable var progressValue:Float = 0.5 {
+        didSet {
+            updateProgress()
+        }
+    }
+    @IBInspectable var progressWidth:CGFloat = 3.0 {
+        didSet {
+            updateSlider()
+        }
+    }
+    @IBInspectable var progressTintColor:UIColor = UIColor.red {
+        didSet {
+            updateSlider()
+        }
+    }
+    @IBInspectable var textColor:UIColor = UIColor.white {
+        didSet {
+            updateLabel()
+        }
+    }
+    @IBInspectable var font:UIFont = UIFont.systemFont(ofSize: 20) {
+        didSet {
+            updateLabel()
+        }
+    }
+    
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func setupView() {
+        updateImageView()
+        updateProgress()
+    }
+    
+    func updateImageView() {
+        iconImageView.frame = CGRect(x: 0, y: bounds.origin.y,
+                                     width: bounds.width, height: bounds.width)
+        addSubview(iconImageView)
+        if let iconImage = iconImage {
+            iconImageView.image = iconImage
+        }
+    }
+    
+    func updateProgress() {
+        updateLabel()
+        updateSlider()
+    }
+    
+    func updateLabel(){
+        progressLabel.frame = CGRect(x: 0,
+                                     y: iconImageView.bounds.width,
+                                     width: bounds.width, height: bounds.width)
+        addSubview(progressLabel)
+        progressLabel.adjustsFontSizeToFitWidth = true
+        progressLabel.textAlignment = .center
+        progressLabel.textColor = textColor
+        progressLabel.font = font
+        var value = Int(progressValue*100.0)
+        value = min(100, value)
+        value = max(0, value)
+        progressLabel.text = "\(value)"
+    }
+    
+    func updateSlider(){
+        let trackRect = CGRect(x: bounds.width/2.0 - CGFloat(progressWidth/2.0),
+                               y: bounds.origin.y+bounds.width*2,
+                               width: CGFloat(progressWidth), height: bounds.height - bounds.width*2.0 - 5.0)
+        let diff = CGFloat(1.0 - progressValue) * trackRect.size.height
+        progress.frame = CGRect(x: trackRect.origin.x,
+                                y: trackRect.origin.y+diff,
+                                width: trackRect.size.width,
+                                height: trackRect.size.height*CGFloat(progressValue))
+        progress.backgroundColor = progressTintColor
+        progressTrack.frame = trackRect
+        progressTrack.backgroundColor = UIColor.darkGray
+        progress.layer.cornerRadius = progressWidth / 2.0
+        progressTrack.layer.cornerRadius = progressWidth / 2.0
+        addSubview(progressTrack)
+        addSubview(progress)
+        
     }
 }
--- PBVerticalProgress/ViewController.swift
+++ PBVerticalProgress/ViewController.swift
@@ -15,11 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var progress: PBVerticalProgress!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changed(_ sender: Any) {
+        if let slider = sender as? UISlider {
+            progress.progressValue = slider.value
+        }
+        
+    }
 
 }
 
